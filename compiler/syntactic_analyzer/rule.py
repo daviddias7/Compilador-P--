d6990c13787f5bd07ae049cc82c6af315b969b32
VitorFCM <vitor.carreramiguel@gmail.com>
2023-07-09 18:06:52 -0300

    Mudando o armazenamento dos seguidores de um list para um set
    Essa mudança facilita a funçao de obtençao dos seguidores, pois o set nao permite valores repetidos

--- compiler/syntactic_analyzer/rule.py
+++ compiler/syntactic_analyzer/rule.py
@@ -2,4 +2,4 @@ class Rule:
     def __init__(self, rules_options_list):
         self.rules_options_list = rules_options_list
         self.first = {}
-        self.follower = []
+        self.follower = set()
